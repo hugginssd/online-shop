ba79f68448e5b8d3d45441cc6e0b5d4843775790
hugginssd <hugginsmafigu@gmail.com>
2019-08-28 13:55:49 +0200

    Added changes to the cart

--- newslettter.php
+++ newslettter.php
@@ -10,7 +10,7 @@
 					<div class="col-md-12">
 					
 						<div class="newsletter">
-							<p>Sign Up for the <strong>OFFERUPDATES</strong></p>
+							<p>Sign Up for the <strong>Offer Updates</strong></p>
 							<form id="offer_form" onsubmit="return false">
 								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
 								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
@@ -20,16 +20,19 @@
                             </div>
 							<ul class="newsletter-follow">
 								<li>
-									<a href="https://github.com/puneethreddyhc"><i class="fa fa-facebook"></i></a>
+									<a href="https://github.com/huggins.t.mafigu"><i class="fa fa-facebook"></i></a>
 								</li>
 								<li>
-									<a href="https://github.com/puneethreddyhc"><i class="fa fa-twitter"></i></a>
+									<a href="https://twitter.com/mambo_chami/"><i class="fa fa-twitter"></i></a>
 								</li>
 								<li>
-									<a href="https://github.com/puneethreddyhc"><i class="fa fa-instagram"></i></a>
+									<a href="https://www.instagram.com/mambo_chami/"><i class="fa fa-instagram"></i></a>
 								</li>
 								<li>
-									<a href="https://github.com/puneethreddyhc"><i class="fa fa-github"></i></a>
+									<a href="https://github.com/hugginssd"><i class="fa fa-github"></i></a>
+								</li>
+								<li>
+									<a href="https://www.linkedin.com/in/huggins-mafigu-0a4a85110/"><i class="fa fa-linkedin"></i></a>
 								</li>
 							</ul>
 						</div>
